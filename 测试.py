02412307cb0a73caaaa86a330614f8cd7b139767
yuying19960318 <1245795090@qq.com>
2025-03-24 15:47:08 +0800

    Add files via upload

---
+++ 测试.py
@@ -0,0 +1,17 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.36 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `financial_user` (
+	`id` bigint (20),
+	`character` varchar (150),
+	`character_code` varchar (150),
+	`create_at` datetime ,
+	`update_at` datetime ,
+	`create_user` varchar (60),
+	`update_user` varchar (60),
+	`yu` int (11)
+); 
