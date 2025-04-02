7253c55dd9940d6a8aac273d1b19b690798a2ae1
yuying19960318 <1245795090@qq.com>
2025-04-02 13:46:02 +0800

    Update route.py
    增加分页场景

--- route.py
+++ route.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
-from crud  import UserCRUD,MenuCRUD,User
-from schemas  import UserCreate ,UserOut,UserLogin,MenuItem,userMenuResponse
+from crud  import UserCRUD,MenuCRUD,UserServer
+from schemas  import UserCreate ,UserOut,UserLogin,MenuItem,userMenuResponse,UserFilter,UserResponse
 from database import get_db
+from typing import Optional,List
 
 
 router = APIRouter()
 #用户注册路由
-@router.post('/register',response_model=UserOut)
+@router.post('/register',response_model=UserOut)    #返回数据需要用UserOut模型验证（输入和都系要验证）
 async def register(
         user: UserCreate,
         db: AsyncSession = Depends(get_db)
@@ -37,9 +38,34 @@ async def login(
 #获取菜单信息路由
 @router.get('/menu')
 async def get_all_menu(
-    db: AsyncSession = Depends(get_db),  # 直接依赖数据库会话
+    db: AsyncSession = Depends(get_db),  # get路由直接获取数据库会话，取MenuCRUD函数的实例，拿到所有菜单
     menu_crud: MenuCRUD = Depends()
 ):
     menus = await menu_crud.get_all_menu(db)  # 调用新方法
     return {"menu": menus}
+
+
+#获取用户信息路由
+@router.get('/users',response_model=list[UserResponse])
+async def get_users(
+        service: UserServer = Depends(),
+        db: AsyncSession = Depends(get_db),
+        username: Optional[str] = Query(None),
+        roles: Optional[List[str]] = Query(None),
+        page: int = Query(1, ge=1),
+        page_size: int = Query(10, ge=1, le=100)
+
+):
+    try:
+        filters=UserFilter(username=username,roles=roles)
+        users = await service.get_user(db, filters, page, page_size)
+        return [
+            UserResponse(
+                username=user.username,
+                roles=[role.name for role in user.roles]
+            )
+            for user in users
+        ]
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
